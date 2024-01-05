66824a36f8bb79234f3c890a1a3601d86af1a5ac
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-05 14:43:55 +0700

    Update seeder:down

--- seeder/1702369471207-CustomerSeed.ts
+++ seeder/1702369471207-CustomerSeed.ts
@@ -4,38 +4,49 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CustomerSeed1702366042225 implements MigrationInterface {
 
-    transaction: false;
+  transaction: false;
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+  public async up(queryRunner: QueryRunner): Promise<void> {
 
-        const f = new Faker({
-            locale: [vi, en]
-        })
+    const f = new Faker({
+      locale: [vi, en]
+    })
+    const addresses = await queryRunner.query(`
+      select id from addresses
+    `)
 
-        for (let i = 0; i <= 10; i++){
-            try {
-            await queryRunner.query(`
+        console.log(addresses)
+
+    for (let i = 0; i <= 1000; i++) {
+      try {
+      const firstName = f.name.firstName();
+      const lastName = f.name.lastName();
+      const addressIndex = f.number.int({min: 0, max: addresses.length-1})
+        await queryRunner.query(`
                 insert into customers
-                (id, name, email, phone, age, address_id, identify, note, "createdAt", "updatedAt") values
+                (id, name, email, phone, dob, address_id, identify, note, "createdAt", "updatedAt", "extension") values
                 (default,
-                '${f.person.fullName()}',
-                '${f.internet.email({})}',
+                '${firstName + lastName}',
+                '${f.internet.email({
+          firstName, lastName
+        })}',
                 '${f.phone.number()}',
-                '${f.number.int({ min: 0, max: 60 })}',
-                default,
+                '${f.date.birthdate().toISOString()}',
+                '${addresses[addressIndex].id}',
                 default,
                 '${f.person.jobDescriptor()}',
                 default,
-                default
+                default,
+                '+84'
                 )
             `)
-            } catch (error) {
-                console.log(error)
-            }
-        }
+      } catch (error) {
+        console.log(error)
+      }
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
 
 }
