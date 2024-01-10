085428312bbae53fe2923bbb6fc36d6864a4ff78
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-10 21:39:54 +0700

    Update cascade

--- migrations/1704521286446-CreateRefTagResourceTag.ts
+++ migrations/1704521286446-CreateRefTagResourceTag.ts
@@ -1,11 +1,12 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateRefTagResourceTag1704521286446
-  implements MigrationInterface
-{
+  implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     return await queryRunner.query(`
         alter table resource_tags add constraint fk_tag foreign key (key) references tags(key) 
+          on update cascade
+          on delete cascade
     `)
   }
 
